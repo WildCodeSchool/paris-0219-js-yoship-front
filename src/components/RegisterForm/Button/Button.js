19000a6ce7f775756500833c2dc0645dbe6b1293
manonab <abelcoindozm@gmail.com>
2019-06-03 14:51:54 +0200

    added register and button

---
+++ src/components/RegisterForm/Button/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '.Button.sccs';
+
+class Button extends React.Component {
+
+    render() {
+        return (
+        <div>
+            <p>
+                <button className="login-button">SUBMIT FORM</button> 
+            </p>
+        </div>
+        )
+    }
+}
+
+export default Button;
