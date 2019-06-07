38df4591bfa2839c7a07743211ef6ab2aa68f6ed
Agueda Perez <aguedapm@yahoo.com>
2019-06-07 11:14:59 +0200

    cleaned code, removed unitilised functions and components, fixed problem with Sidenav not closing on click on Sidenav items

--- src/components/Header/Burger/ResponsiveNavbar.js
+++ src/components/Header/Burger/ResponsiveNavbar.js
@@ -3,35 +3,27 @@ import React from 'react';
 import Navbar from './Navbar';
 import SideNav from './SideNav';
 import './ResponsiveNavbar.scss';
-import BurgerButton from './BurgerButton';
+
 
 class ResponsiveNavbar extends React.Component {
   state = {
     sideDrawerOpen: false,
   };
 
-  drawerToggleClickHandler = () => {
-    // this.setState({sideDrawerOpen: !this.state.sideDrawerOpen})
+  drawerClickHandler = () => {
     this.setState((prevState) => {
       return{sideDrawerOpen: !prevState.sideDrawerOpen}
     });
   };
 
   render () {
-    let sideDrawer;
-      if (this.state.sideDrawerOpen) {
-        sideDrawer = <SideNav />
-      }
 
     return (
       <div>
         <header className="toolbar"> 
-          <SideNav show={this.state.sideDrawerOpen}/>
+          <SideNav show={this.state.sideDrawerOpen} drawerClickHandler={this.drawerClickHandler} />
           <nav className="toolbar_navigation">
-            <div className="toolbar_toggle-button">
-              <BurgerButton click={this.props.drawerClickHandler}/>
-            </div>
-            <Navbar drawerClickHandler={this.drawerToggleClickHandler}/>
+            <Navbar drawerClickHandler={this.drawerClickHandler}/>
           </nav>
         </header>
       </div>
--- src/components/Header/Burger/SideNav.js
+++ src/components/Header/Burger/SideNav.js
@@ -3,27 +3,26 @@ import './SideNav.scss';
 import {NavLink } from 'react-router-dom';
 import Tab from './Tab';
 
-const SideNav = props => {
-    let drawerClass = 'side-drawer';
-    if (props.show) {
-        drawerClass ='side-drawer open';
-    }
+const SideNav = ({show, drawerClickHandler}) => {
+    let drawerClass = show? 'side-drawer open' : 'side-drawer';
+
     return (
+        
     <nav className={drawerClass}>
         <ul>
             <li>
-                <NavLink to="/tracking" >
-                <Tab icon="map-marker-alt" tab="Tracking"/>
+                <NavLink to="/tracking" onClick={drawerClickHandler} >
+                    <Tab icon="map-marker-alt" tab="Tracking"/>
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/login">
-                <Tab icon="user-tie" tab="Login"/>
+                <NavLink to="/login" onClick={drawerClickHandler} >
+                    <Tab icon="user-tie" tab="Login"/>
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/register">
-                <Tab icon="user-plus" tab="Register"/>
+                <NavLink to="/register" onClick={drawerClickHandler}>
+                    <Tab icon="user-plus" tab="Register"/>
                 </NavLink>
             </li>
         </ul>
