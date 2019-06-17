73a19fa95b324021c4ab1cb88b736940183d12f2
manonab <abelcoindozm@gmail.com>
2019-06-17 16:34:54 +0200

    created a new component ScrollToTopRoute when onclick of register, login and tracking the page is opening on the top. change in the app.js route by ScrollingToTopRoute for the path of login, tracking, register and home.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Route, Switch} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
 
 import Header from '../components/Header/Header'
 import Home from './../screen/Home'
 import Login from './../screen/Login'
 import Tracking from './../screen/Tracking'
 import Register from './../screen/Register'
+import ScrollToTopRoute from './Header/ScrollToTop'
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/tracking" component={Tracking} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <ScrollToTopRoute exact path="/" component={Home} />
+        <ScrollToTopRoute path="/tracking" component={Tracking} />
+        <ScrollToTopRoute path="/login" component={Login} />
+        <ScrollToTopRoute path="/register" component={Register} />
       </Switch>
+
     </div>
    
   );
