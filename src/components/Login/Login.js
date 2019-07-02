99ba39cc05eead2741de0aff03d51739d241dd86
Arnaud Malanda <arnaud.malanda@gmail.com>
2019-07-02 11:58:00 +0200

    [Clean] Indent components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,31 +7,27 @@ class Login extends React.Component {
 
     render() {
         return (
-        
-            
             <form className="container-form-login"> 
             {/* label + input + picto email */}  
-            <p>
-                <label className="label-email-login" for="email">Email adress</label><br />
-                <div className="group-email"> 
-            <InputGroupAddon addonType="prepend">@</InputGroupAddon><br />
-            <Input className="input-email-login" name="Email address" type="email" placeholder="Enter your email address" /></div>
-            </p>
+                <p>
+                    <label className="label-email-login" for="email">Email adress</label><br />
+                    <div className="group-email"> 
+                        <InputGroupAddon addonType="prepend">@</InputGroupAddon><br />
+                        <Input className="input-email-login" name="Email address" type="email" placeholder="Enter your email address" />
+                    </div>
+                </p>
             {/* label + input + password */}  
-            <p>
-            <label className="label-password-login" for="password">Password</label><br />
-            <Input className="input-password-login" name="Password" type="text" placeholder="Enter your password"/>
-            </p>
+                <p>
+                    <label className="label-password-login" for="password">Password</label><br />
+                    <Input className="input-password-login" name="Password" type="text" placeholder="Enter your password"/>
+                </p>
             {/* checkbox check me out  */}
-            <span className="group-check">
-            <Input type="checkbox" /><label> Check me out</label><br/>
-            </span>
-            <span className="button-login"><Button text="SUBMIT"/></span>
+                <span className="group-check">
+                <Input type="checkbox" /><label> Check me out</label><br/>
+                </span>
+                <span className="button-login"><Button text="SUBMIT"/></span>
             {/* boutton submt */}
-        </form>
-       
-        
-        
+            </form>
         )
     }
 }
