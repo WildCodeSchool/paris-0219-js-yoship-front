540cba63793adcfe8c2a414b56e0ec9f0e725254
Rémy Jacob <rjpro77@outlook.fr>
2019-07-23 15:03:16 +0200

    added label in the upload

--- src/components/Mesdocuments/MesDocuments.js
+++ src/components/Mesdocuments/MesDocuments.js
@@ -26,33 +26,32 @@ class Mesdocuments extends React.Component {
   state = {
     result: [],
     loading: true,
-    selectFile:'',
-    identity:''
+    selectFile: '',
+    dataPapers: ''
   }
-    getIdentity = () => {
-      const uuid = localStorage.getItem("uuid")
-      const token = localStorage.getItem("token")
-      axios({
-        method: "GET",
-        url: `http://localhost:${config.port}/users/${uuid}/driverPapers`,
-        headers: {
-          "x-access-token": token
-        }
-      })
+  getDataPapers = () => {
+    const uuid = localStorage.getItem("uuid")
+    const token = localStorage.getItem("token")
+    axios({
+      method: "GET",
+      url: `http://localhost:${config.port}/users/${uuid}/driverPapers`,
+      headers: {
+        "x-access-token": token
+      }
+    })
       .then(res => {
-        console.log(res.data[0].identityCard)
         this.setState({
-          identity: res.data[0].identityCard
+          dataPapers: res.data[0]
         })
       }).catch(res => {
         console.log(res)
       })
-    }
+  }
 
 
   changeHandler = async (event) => {
     await this.setState({
-     selectFile: event.target.files[0]
+      selectFile: event.target.files[0]
     })
     console.log(this.state.selectFile)
   }
@@ -63,7 +62,7 @@ class Mesdocuments extends React.Component {
     const token = localStorage.getItem("token")
     const fileType = event.target.name
     const formData = new FormData()
-    formData.append(fileType,this.state.selectFile)
+    formData.append(fileType, this.state.selectFile)
     axios({
       method: "PUT",
       url: `http://localhost:${config.port}/users/${uuid}/driverPapers/${fileType}`,
@@ -78,7 +77,7 @@ class Mesdocuments extends React.Component {
       })
       .catch(error => {
         console.log(error.response)
-    });
+      });
   }
 
   getData = () => {
@@ -102,7 +101,7 @@ class Mesdocuments extends React.Component {
 
   componentDidMount = () => {
     this.getData()
-    this.getIdentity()
+    this.getDataPapers()
     console.log(this.state.identity)
   }
 
@@ -116,7 +115,7 @@ class Mesdocuments extends React.Component {
       const data = this.state.result[0]
       // const dataDrivers = this.state.dataDrivers[0]
       return (
-        
+
         <section id="project" className="project-section bg-light">
           <Container>
             <div className="row align-items-center no-gutters mb-4 mb-lg-5">
@@ -135,7 +134,7 @@ class Mesdocuments extends React.Component {
                 <CardTitle><h4> <img src={blacktiebrands} className="phonesquarealtsolid" alt="logo" /> {data.name} {data.firstname} (User ID:2323)</h4></CardTitle>
                 <CardText><img src={phonesquarealtsolid} className="phonesquarealtsolid" alt="logo" /> Your phone : {data.phone}</CardText>
                 <CardText><img src={atsolid} className="phonesquarealtsolid" alt="logo" /> Email Addres : {data.mail} </CardText>
-                <CardText><img src={usertagsolid} className="phonesquarealtsolid" alt="logo"/> Status : check </CardText>
+                <CardText><img src={usertagsolid} className="phonesquarealtsolid" alt="logo" /> Status : check </CardText>
               </Col>
 
 
@@ -150,27 +149,36 @@ class Mesdocuments extends React.Component {
 
 
 
-              <Col xl="4" lg="4">
               <form enctype="multipart/form-data" method="PUT" >
-               
-                <input type="file" name="identityCard" onChange={this.changeHandler} />
-                <button type="button" name="identityCard" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
+                <div>
 
-                <input type="file" name="proofOfResidence" onChange={this.changeHandler} />
-                <button type="button" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
 
-                <input type="file" name="rib" onChange={this.changeHandler} />
-                <button type="button" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
+                  <Col xl="4" lg="4">
+                    <label htmlFor="identityCard">CNI:</label>
+                    <input type="file" name="identityCard" onChange={this.changeHandler} />
+                    <button type="button" name="identityCard" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
+                    <label htmlFor="proofOfResidence">Justifcatif de domicile</label>
+                    <input type="file" name="proofOfResidence" onChange={this.changeHandler} />
+                    <button type="button" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
+                  </Col>
 
-                <input type="file" name="driverLicense" onChange={this.changeHandler} />
-                <button type="button" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
+                   
+
+
+                </div>
+                <div>
+
+                  <Col xl="4" lg="4">
+                    <label htmlFor="rib">RIB</label>
+                    <input type="file" name="rib" onChange={this.changeHandler} />
+                    <button type="button" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
+                    <label htmlFor="driverLicense">Permis de conduire</label>
+                    <input type="file" name="driverLicense" onChange={this.changeHandler} />
+                    <button type="button" className="btn btn-success btn-block" onClick={this.handleSubmit}>Upload</button>
+                  </Col>
+                </div>
               </form>
-              </Col>
-            
-            </div>
-            <div>
-              <p>{this.state.identity}</p>
-              
+
             </div>
           </Container>
 
