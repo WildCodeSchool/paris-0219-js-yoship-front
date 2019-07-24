baa799b792331beceb94f7eb27b536f42a60eee8
Monia-L <monia.laoufi@gmail.com>
2019-07-24 18:44:38 +0200

    [code] added navbar in profilUpdate

--- src/screen/ProfilUpdate.js
+++ src/screen/ProfilUpdate.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from '../components/Footer/Footer'
+import Header from "../components/Header/Header";
 import ProfilUpdate from '../components/Monprofil/ProfilEdit/ProfilUpdate'
 
 const MonProfilUpdate = () => {
     return (
         <div>
+            <Header />
             <ProfilUpdate />
             <Footer />
         </div>
